a3c82d0c8f55906105b5a3cd8bbc1dd7d8b2332b
luanshu <luanshu.cqj@raycloud.com>
2022-02-18 22:17:44 +0800

    feat: change file

---
+++ src/router/routes.js
@@ -0,0 +1,17 @@
+import Home from "../views/home/home.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/about/about.vue"),
+  },
+];
+
+export default routes;
